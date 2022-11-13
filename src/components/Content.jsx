4329ecaa4f25f6ce1b6642273f409082b5af5a51
Valentin <V.Kovalenko.prog@gmail.com>
2022-11-13 14:26:01 +0200

    change media

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -36,7 +36,7 @@ function Content() {
             <a href="https://github.com/ValentinProg">
               <Github />
             </a>
-            <a href="https://t.me/ValentinKovalenko">
+            <a href="https://www.linkedin.com/in/%D0%B2%D0%B0%D0%BB%D0%B5%D0%BD%D1%82%D0%B8%D0%BD-%D0%BA%D0%BE%D0%B2%D0%B0%D0%BB%D0%B5%D0%BD%D0%BA%D0%BE-705b8b256/">
             <LinkedIn />
             </a>
             <a href="https://t.me/ValentinKovalenko">
